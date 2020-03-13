f6d6c13df132e83ac3cbd176c050d678a7cfbb1b
cedmail <cmailleux@jahia.com>
2020-03-13 12:15:46 -0400

    BACKLOG-12752: Add requiredPermissions for category/repository explorer, update Administration to use nodeInfo hooks as in jahia-ui-root

--- src/javascript/RepositoryExplorer/RepositoryExplorer.routes.jsx
+++ src/javascript/RepositoryExplorer/RepositoryExplorer.routes.jsx
@@ -5,6 +5,8 @@ import RepositoryExplorer from './RepositoryExplorer';
 
 registry.add('primary-nav-item', 'repositoryexplorerNavGroup', {
     targets: ['nav-root-top:6'],
+    requiredPermission: 'repositoryExplorer',
+    requiredPermissionPath: '/',
     render: () => <RepositoryExplorer/>
 });
 
@@ -12,5 +14,7 @@ registry.add('route', 'repositoryexplorer', {
     targets: ['nav-root-top'],
     path: REConstants.ROUTE,
     defaultPath: REConstants.ROUTE,
+    requiredPermission: 'repositoryExplorer',
+    requiredPermissionPath: '/',
     render: () => <IframeRenderer url={`${window.contextJsParameters.contextPath}/engines/manager.jsp?conf=repositoryexplorer-anthracite&lang=${window.contextJsParameters.lang}`}/>
 });
