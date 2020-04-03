03025de2e55ce67c6571ce18014db5d4c7be2057
Francois Pral <fpral@jahia.com>
2020-04-03 14:39:46 +0200

    BACKLOG-13096: Updated icon for Repository Explorer entry in level 1 + fixed name in French and added missing German rb file

--- src/javascript/RepositoryExplorer/RepositoryExplorer.jsx
+++ src/javascript/RepositoryExplorer/RepositoryExplorer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
 import {PrimaryNavItem} from '@jahia/moonstone';
-import FolderSpecial from '@jahia/moonstone/dist/icons/FolderSpecial';
+import Explore from '@jahia/moonstone/dist/icons/Explore';
 import {useHistory} from 'react-router';
 import REConstants from './RepositoryExplorer.constants';
 
@@ -11,7 +11,7 @@ const RepositoryExplorer = () => {
 
     return (
         <PrimaryNavItem role="repository-explorer-menu-item"
-                        icon={<FolderSpecial/>}
+                        icon={<Explore/>}
                         label={t('label')}
                         isSelected={history.location.pathname.startsWith(REConstants.ROUTE)}
                         onClick={() => history.push(REConstants.ROUTE)}/>
